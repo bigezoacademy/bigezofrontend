cb3da96646bb9eb5c628a51b142deaf6d3822007
ugfire <ugfirecompany@gmail.com>
2025-01-09 01:43:13 +0300

    minor edit

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-userId: any=localStorage.getItem("userId");
+userId: any=localStorage.getItem("id");
 
 }
