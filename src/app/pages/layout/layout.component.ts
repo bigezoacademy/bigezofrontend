f5e9e3f7f753fdc73e0cd87b6b1c8c075c640f51
ugfire <ugfirecompany@gmail.com>
2025-01-10 09:50:40 +0300

    complex sidenav responsive black sidenav

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,27 +1,27 @@
 
+import { CommonModule } from '@angular/common';
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
-  constructor(private router:Router,private renderer: Renderer2){
+  sidebarVisible: boolean = false;
 
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  toggleSidebar(): void {
+    this.sidebarVisible = !this.sidebarVisible;
   }
 accounttype:any=localStorage.getItem("Role");
 
 ngOnInit(): void {
-  const script = this.renderer.createElement('script');
-  script.src = 'src/index.js';
-  script.onload = () => {
-    console.log('index.js loaded');
-  };
-  this.renderer.appendChild(document.body, script);
+
 }
   terms():any{
 this.router.navigateByUrl("terms");
