9d51ed3eb9aa5cab9a4291dd54d09fd4f52e3806
kidsclubkampala <ugline@kidsclubkampala.org>
2025-01-27 15:45:38 +0300

    modified the fields of the transactions

--- src/app/pages/sidenav-functions/transactions/transactions.component.ts
+++ src/app/pages/sidenav-functions/transactions/transactions.component.ts
@@ -14,173 +14,160 @@ export class TransactionsComponent {
   currentPage: number = 1;
   itemsPerPage: number = 5;
   totalRows: number = 0;
+ data= [
+  {  
+      "StudentId": 1,
+      "StudentName": "Aisha Namukasa",  
+      "Amount": 150000,  
+      "order_tracking_id": "b945e4af-80a5-4ec1-8706-e03f8332fb04",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 1 Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 08:30 AM",
+      "Level": 6,  
+      "Term": "Term 1",  
+      "Year": 2025,
+      "schoolAdminId": 1
+  },
+  {  
+      "StudentId": 2,
+      "StudentName": "Sylvia Nakato",  
+      "Amount": 175000,  
+      "order_tracking_id": "m901e4af-80a5-4ec1-8706-e03f8332fb14",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 1 Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 12:15 PM",
+      "Level": 6,  
+      "Term": "Term 1",  
+      "Year": 2025,
+      "schoolAdminId": 1
+  },
+  {  
+      "StudentId": 3,
+      "StudentName": "Josephine Mirembe",  
+      "Amount": 240000,  
+      "order_tracking_id": "n234e4af-80a5-4ec1-8706-e03f8332fb15",  
+      "Reason": "School Fees",  
+      "Description": "Paid Full Year Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 12:30 PM",
+      "Level": 7,  
+      "Term": "Full Year",  
+      "Year": 2025,
+      "schoolAdminId": 2
+  },
+  {  
+      "StudentId": 4,
+      "StudentName": "Kenneth Ochieng",  
+      "Amount": 210000,  
+      "order_tracking_id": "o345e4af-80a5-4ec1-8706-e03f8332fb16",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 2 Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 12:45 PM",
+      "Level": 6,  
+      "Term": "Term 2",  
+      "Year": 2025,
+      "schoolAdminId": 3
+  },
+  {  
+      "StudentId": 5,
+      "StudentName": "Leah Naggayi",  
+      "Amount": 220000,  
+      "order_tracking_id": "p456e4af-80a5-4ec1-8706-e03f8332fb17",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 3 Fees",  
+      "statusCode": "400",  
+      "status": "failed",  
+      "Time": "2025-01-27 01:00 PM",
+      "Level": 7,  
+      "Term": "Term 3",  
+      "Year": 2025,
+      "schoolAdminId": 1
+  },
+  {  
+      "StudentId": 6,
+      "StudentName": "Tom Kasyaba",  
+      "Amount": 195000,  
+      "order_tracking_id": "q567e4af-80a5-4ec1-8706-e03f8332fb18",  
+      "Reason": "School Fees",  
+      "Description": "Paid Registration Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 01:15 PM",
+      "Level": 6,  
+      "Term": "Registration",  
+      "Year": 2025,
+      "schoolAdminId": 2
+  },
+  {  
+      "StudentId": 7,
+      "StudentName": "Esther Okot",  
+      "Amount": 270000,  
+      "order_tracking_id": "r678e4af-80a5-4ec1-8706-e03f8332fb19",  
+      "Reason": "School Fees",  
+      "Description": "Paid Full Year Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 01:30 PM",
+      "Level": 7,  
+      "Term": "Full Year",  
+      "Year": 2025,
+      "schoolAdminId": 3
+  },
+  {  
+      "StudentId": 8,
+      "StudentName": "Emmanuel Kamya",  
+      "Amount": 250000,  
+      "order_tracking_id": "s789e4af-80a5-4ec1-8706-e03f8332fb20",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 1 Fees",  
+      "statusCode": "500",  
+      "status": "failed",  
+      "Time": "2025-01-27 01:45 PM",
+      "Level": 6,  
+      "Term": "Term 1",  
+      "Year": 2025,
+      "schoolAdminId": 2
+  },
+  {  
+      "StudentId": 9,
+      "StudentName": "Nina Nabirye",  
+      "Amount": 230000,  
+      "order_tracking_id": "t890e4af-80a5-4ec1-8706-e03f8332fb21",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 2 Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 02:00 PM",
+      "Level": 6,  
+      "Term": "Term 2",  
+      "Year": 2025,
+      "schoolAdminId": 1
+  },
+  {  
+      "StudentId": 10,
+      "StudentName": "George Nsamba",  
+      "Amount": 190000,  
+      "order_tracking_id": "u901e4af-80a5-4ec1-8706-e03f8332fb22",  
+      "Reason": "School Fees",  
+      "Description": "Paid Term 3 Fees",  
+      "statusCode": "200",  
+      "status": "successful",  
+      "Time": "2025-01-27 02:15 PM",
+      "Level": 7,  
+      "Term": "Term 3",  
+      "Year": 2025,
+      "schoolAdminId": 3
+  }
+]
 
-  data = [
-     
-      {  
-          "StudentName": "Aisha Namukasa",  
-          "Amount": 150000,  
-          "order_tracking_id": "b945e4af-80a5-4ec1-8706-e03f8332fb04",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 1 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 08:30 AM"  
-      },  
-      {  
-          "StudentName": "John Okello",  
-          "Amount": 300000,  
-          "order_tracking_id": "c123e4af-80a5-4ec1-8706-e03f8332fb04",  
-          "Reason": "School Fees",  
-          "Description": "Paid Full Year Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 09:00 AM"  
-      },  
-      {  
-          "StudentName": "Mary Nansubuga",  
-          "Amount": 200000,  
-          "order_tracking_id": "d456e4af-80a5-4ec1-8706-e03f8332fb05",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 2 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 09:30 AM"  
-      },  
-      {  
-          "StudentName": "Samuel Kiwalabye",  
-          "Amount": 250000,  
-          "order_tracking_id": "e789e4af-80a5-4ec1-8706-e03f8332fb06",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 3 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 10:00 AM"  
-      },  
-      {  
-          "StudentName": "Fatima Alimansi",  
-          "Amount": 180000,  
-          "order_tracking_id": "f012e4af-80a5-4ec1-8706-e03f8332fb07",  
-          "Reason": "School Fees",  
-          "Description": "Paid Registration Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 10:15 AM"  
-      },  
-      {  
-          "StudentName": "David Kato",  
-          "Amount": 220000,  
-          "order_tracking_id": "g345e4af-80a5-4ec1-8706-e03f8332fb08",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 1 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 10:30 AM"  
-      },  
-      {  
-          "StudentName": "Grace Nankya",  
-          "Amount": 160000,  
-          "order_tracking_id": "h678e4af-80a5-4ec1-8706-e03f8332fb09",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 2 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 10:45 AM"  
-      },  
-      {  
-          "StudentName": "Peter Muwanguzi",  
-          "Amount": 270000,  
-          "order_tracking_id": "i901e4af-80a5-4ec1-8706-e03f8332fb10",  
-          "Reason": "School Fees",  
-          "Description": "Paid Full Year Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 11:00 AM"  
-      },  
-      {  
-          "StudentName": "Sarah Nansubuga",  
-          "Amount": 190000,  
-          "order_tracking_id": "j234e4af-80a5-4ec1-8706-e03f8332fb11",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 3 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 11:15 AM"  
-      },  
-      {  
-          "StudentName": "Isaac Kiggundu",  
-          "Amount": 300000,  
-          "order_tracking_id": "k567e4af-80a5-4ec1-8706-e03f8332fb12",  
-          "Reason": "School Fees",  
-          "Description": "Paid Full Year Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 11:30 AM"  
-      },  
-      {  
-          "StudentName": "Lydia Nandawula",  
-          "Amount": 140000,  
-          "order_tracking_id": "l890e4af-80a5-4ec1-8706-e03f8332fb13",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 1 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 11:45 AM"  
-      },  
-      {  
-          "StudentName": "Brian Ssemakula",  
-          "Amount": 230000,  
-          "order_tracking_id": "m123e4af-80a5-4ec1-8706-e03f8332fb14",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 2 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 12:00 PM"  
-      },  
-      {  
-          "StudentName": "Esther Mugisha",  
-          "Amount": 210000,  
-          "order_tracking_id": "n456e4af-80a5-4ec1-8706-e03f8332fb15",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 3 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 12:15 PM"  
-      },  
-      {  
-          "StudentName": "Michael Obote",  
-          "Amount": 175000,  
-          "order_tracking_id": "o789e4af-80a5-4ec1-8706-e03f8332fb16",  
-          "Reason": "School Fees",  
-          "Description": "Paid Registration Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 12:30 PM"  
-      },  
-      {  
-          "StudentName": "Nina Kiggundu",  
-          "Amount": 195000,  
-          "order_tracking_id": "p012e4af-80a5-4ec1-8706-e03f8332fb17",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 1 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 12:45 PM"  
-      },  
-      {  
-          "StudentName": "Tom Muwanga",  
-          "Amount": 280000,  
-          "order_tracking_id": "q345e4af-80a5-4ec1-8706-e03f8332fb18",  
-          "Reason": "School Fees",  
-          "Description": "Paid Full Year Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 01:00 PM"  
-      },  
-      {  
-          "StudentName": "Diana Kanyange",  
-          "Amount": 165000,  
-          "order_tracking_id": "r678e4af-80a5-4ec1-8706-e03f8332fb19",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 2 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 01:15 PM"  
-      },  
-      {  
-          "StudentName": "Victor Nsubuga",  
-          "Amount": 240000,  
-          "order_tracking_id": "s901e4af-80a5-4ec1-8706-e03f8332fb20",  
-          "Reason": "School Fees",  
-          "Description": "Paid Term 3 Fees",  
-          "status": "200",  
-          "Time": "2025-01-27 01:30 PM"  
-      }  
-  
-  ];
+
 
  
 
